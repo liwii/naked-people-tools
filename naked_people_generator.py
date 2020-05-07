fe7507f33bb092c661e1a01d09e9e744dceef0db
Koki Ryu <liukoki@gmail.com>
2020-05-07 18:59:30 +0900

    Scale & translation

--- naked_people_generator.py
+++ naked_people_generator.py
@@ -16,7 +16,7 @@ class NakedPeopleGenarator:
         self.smplh_path = smplh_path
         self.dmpl_path = dmpl_path
 
-    def nakedgen(self, output_file, subject_id, pose_id, bg_image=None, bg_color='white', color='grey', rotation=0, imw=300, imh=300, frame_skip=2):
+    def nakedgen(self, output_file, subject_id, pose_id, bg_image=None, bg_color='white', color='grey', rotation=0, imw=300, imh=300, frame_skip=2, scale=1, translation = [0, 0]):
         device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
         npz_bdata_path = "{}/Subject_{}_F_MoSh/Subject_{}_F_{}_poses.npz".format(self.bmlmovi_path, subject_id, subject_id, pose_id)
         bdata = np.load(npz_bdata_path)
@@ -53,6 +53,7 @@ class NakedPeopleGenarator:
         else:
             texture = None
         wall_mesh = trimesh.Trimesh(vertices=c2c(wall), faces=wall_face, visual=texture, vertex_colors=np.tile(colors[bg_color], (4, 1)))
+        translation = np.array(translation)
 
         for fId in range(0, len(poses), frame_skip):
             root_orient = poses[fId:fId + 1, :3]
@@ -63,6 +64,12 @@ class NakedPeopleGenarator:
             body_mesh_wfingers = trimesh.Trimesh(vertices=c2c(body.v[0]), faces=faces, vertex_colors=np.tile(colors[color], (6890, 1)))
             apply_mesh_tranfsormations_([body_mesh_wfingers], trimesh.transformations.rotation_matrix(- pi / 1.9, (1, 0, 0)))
             apply_mesh_tranfsormations_([body_mesh_wfingers], trimesh.transformations.rotation_matrix(rotation, (0, 1, 0)))
+            basepoint = body_mesh_wfingers.vertices[:, 2].max().item()
+            measure = (body_mesh_wfingers.vertices[:, 1].max().item() - body_mesh_wfingers.vertices[:, 1].min().item())
+            body_mesh_wfingers.vertices[:, 2] -= basepoint
+            body_mesh_wfingers.vertices *= scale
+            body_mesh_wfingers.vertices[:, 2] += basepoint
+            body_mesh_wfingers.vertices[:, :2] += translation * measure
             mv.set_static_meshes([body_mesh_wfingers, wall_mesh])
             body_image_wfingers = mv.render(render_wireframe=False)
             writer.writeFrame(body_image_wfingers)
